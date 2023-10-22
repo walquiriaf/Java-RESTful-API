2d6370a1abed77d8a4bb034413f73ffd5f3acf76
Walquíria Ferreira <walquiriaferreira03@gmail.com>
2023-10-22 17:10:39 -0300

    conserto do precision e scale

--- src/main/java/restfulApi/javarestfulapi/domain/model/Feature.java
+++ src/main/java/restfulApi/javarestfulapi/domain/model/Feature.java
@@ -1,9 +1,6 @@
 package restfulApi.javarestfulapi.domain.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 
 @Entity(name = "tb_feature")
--- src/main/java/restfulApi/javarestfulapi/domain/model/News.java
+++ src/main/java/restfulApi/javarestfulapi/domain/model/News.java
@@ -1,9 +1,6 @@
 package restfulApi.javarestfulapi.domain.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity(name = "td_news")
 public class News extends BaseIcon{
